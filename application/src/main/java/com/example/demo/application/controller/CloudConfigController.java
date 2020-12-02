020fe23de0724b90002314e28061be6c702ca2b0
Rashad <rashadjavad@gmail.com>
2020-12-02 12:12:47 +0100

    spring cloud config support added

---
+++ application/src/main/java/com/example/demo/application/controller/CloudConfigController.java
@@ -0,0 +1,20 @@
+package com.example.demo.application.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/config")
+public class CloudConfigController {
+    @Value("${message : Default message}")
+    private String message;
+
+    @GetMapping(value = "/message")
+    public String getConfigMessage() {
+        return message;
+    }
+}
